22e3d29486211cd3505e7614aa02d5712ab3963a
nigelnh <121311174+nigelnh@users.noreply.github.com>
2024-07-09 10:48:34 +0700

    Initial Commit

---
+++ todo-list/todo_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  todo_listApp.swift
+//  todo-list
+//
+//  Created by Nhân Nguyễn on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct todo_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
